043097d49ab7b5f293b59fef8feb8b39c52d79c8
weihao-Tey <142696206+weihao-Tey@users.noreply.github.com>
2024-04-11 09:54:22 +0800

    Update ID_FIELDS.php

--- ID_FIELDS.php
+++ ID_FIELDS.php
@@ -9,18 +9,7 @@
 defined( 'ABSPATH' ) or exit;
 
 // Hook into Ninja Forms to register the custom field
-add_action( 'ninja_forms_register_fields', 'register_my_custom_field_type' );
 add_action('wp_enqueue_scripts', 'enqueue_external_id_script');
-
-function register_my_custom_field_type($actions) {
-    require_once plugin_dir_path( __FILE__ ) . 'includes/Fields/NRIC.php';
-    require_once plugin_dir_path( __FILE__ ) . 'includes/Fields/UEN.php';
-
-    $actions['nric'] = new NF_Field_NRIC(); 
-    $actions['uen'] = new NF_Field_UEN(); 
-
-    return $actions;
-}
 
 function enqueue_external_id_script() {
     // Only enqueue the script if Ninja Forms is being used on the page
